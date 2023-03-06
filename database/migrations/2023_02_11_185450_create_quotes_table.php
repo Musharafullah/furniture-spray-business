4ca25e3dd7bc77263bfb77941fcc69791ca14056
Musharafullah <musharafullah210565@gmail.com>
2023-03-06 09:51:59 +0500

    createquote23

--- database/migrations/2023_02_11_185450_create_quotes_table.php
+++ database/migrations/2023_02_11_185450_create_quotes_table.php
@@ -36,22 +36,22 @@ return new class extends Migration
 
             // end status
 
-            $table->string('delivery_option')->default('-');
-            $table->double('collected')->nullable();
-            $table->double('delivered')->nullable();
-            $table->longText('comment')->nullable();
-            $table->longText('internal_comment')->nullable();
-            $table->integer('hide_collect')->default(1);
-            $table->integer('hide_delivered')->default(1);
-            $table->integer('total_net_status')->default(1);
-            $table->integer('total_vat_status')->default(1);
-            $table->integer('gross_total_status')->default(1);
-            $table->integer('net_price_status')->default(1);
-            $table->integer('discount_status')->default(1);
-            $table->integer('product_price_status')->default(1);
-            $table->string('hidden_price')->default('Option_3(hide_all_price_column_and_discount_including_gross_total,vat,total net)');
-            $table->string('billing_postal_code')->nullable();
-            $table->string('status')->default('draft');
+            // $table->string('delivery_option')->default('-');
+            // $table->double('collected')->nullable();
+            // $table->double('delivered')->nullable();
+            // $table->longText('comment')->nullable();
+            // $table->longText('internal_comment')->nullable();
+            // $table->integer('hide_collect')->default(1);
+            // $table->integer('hide_delivered')->default(1);
+            // $table->integer('total_net_status')->default(1);
+            // $table->integer('total_vat_status')->default(1);
+            // $table->integer('gross_total_status')->default(1);
+            // $table->integer('net_price_status')->default(1);
+            // $table->integer('discount_status')->default(1);
+            // $table->integer('product_price_status')->default(1);
+            // $table->string('hidden_price')->default('Option_3(hide_all_price_column_and_discount_including_gross_total,vat,total net)');
+            // $table->string('billing_postal_code')->nullable();
+            // $table->string('status')->default('draft');
 
 
             $table->timestamps();
